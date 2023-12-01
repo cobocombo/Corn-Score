e980e572273bb83a6041afc316eaee5f6e22aaf1
Colton Boyd <coltonboyd503@icloud.com>
2023-11-30 23:01:02 -0500

    Added Web folder to Android project.

--- Source/Mobile/Android/app/src/main/java/com/cobo/cornscore/MainActivity.kt
+++ Source/Mobile/Android/app/src/main/java/com/cobo/cornscore/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.cobo.cornscore
-
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import androidx.browser.customtabs.CustomTabsIntent
+import android.net.Uri
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val webView = WebView(this)
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.allowFileAccess = true
+        webView.loadUrl("file:///android_asset/Web/index.html")
+
+        val messageHandler = AndroidMessageHandler(this, this)
+        webView.addJavascriptInterface(messageHandler, "androidMessageHandler")
+        setContentView(webView)
+    }
+}
+
+class AndroidMessageHandler(private val activity: AppCompatActivity, private val context: Context)
+{
+    @JavascriptInterface
+    fun debug(message: String)
+    {
+        Log.d("Corn Score", message)
+    }
+
+    @JavascriptInterface
+    fun openInCustomTabs(url: String)
+    {
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(context, Uri.parse(url))
+        Log.d("Corn Score", url)
     }
 }
