96444e698cee06e57b2c7a984afdd51305d9f734
Colton Boyd <coltonboyd@Coltons-MBP.lan>
2025-01-20 14:50:33 -0500

    Created default iOS project.

---
+++ Code/iOS/Corn-Score/Corn-Score/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Corn-Score
+//
+//  Created by Colton Boyd on 1/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
